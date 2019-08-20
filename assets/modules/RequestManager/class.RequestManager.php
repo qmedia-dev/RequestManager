55ffe8945937b0b3ad14db62588a4bf8c80cc7ab
cgehuzi <mail.cgehuzi@gmail.com>
2019-08-20 15:03:10 +0300

    [ class ] - add class-file

---
+++ assets/modules/RequestManager/class.RequestManager.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!defined('MODX_BASE_PATH')) {
+    die('What are you doing? Get out of here!');
+}
+
+class RequestManager
+{
+    public function __construct($modx, $config)
+    {
+        $this->modx = $modx;
+        $this->config = $config;
+
+        $this->module_id = (int) $_GET['id'];
+        $this->module_url = 'index.php?a=112&id=' . $this->moduleid;
+        $this->theme = $this->modx->config['manager_theme'] ? $this->modx->config['manager_theme'] : 'default';
+        $this->jquery_path = $this->modx->config['mgr_jquery_path'] ? $this->modx->config['mgr_jquery_path'] : 'media/script/jquery/jquery.min.js';
+    }
+
+    public function getFileContents($file)
+    {
+        if (empty($file)) {
+            return false;
+        } else {
+            $file = MODX_BASE_PATH . 'assets/modules/RequestManager/templates/' . $file;
+            $contents = file_get_contents($file);
+            return $contents;
+        }
+    }
+}
--- assets/modules/RequestManager/module.RequestManager.php
+++ assets/modules/RequestManager/module.RequestManager.php
@@ -3,3 +3,24 @@
 if (!defined('MODX_BASE_PATH')) {
     die('What are you doing? Get out of here!');
 }
+
+require_once MODX_BASE_PATH . 'assets/modules/RequestManager/class.RequestManager.php';
+
+// TODO: заготовка под будующую конфигурацию (для передачи в Class)
+$module_config = array();
+
+$module = new RequestManager($modx, $module_config);
+
+// генерируем фронт
+$tpl = $module->getFileContents('main.html');
+
+// плейсхолдеры для фронта
+$placeholders = array(
+    'theme'             => $module->theme,
+    'module_id'         => $module->module_id,
+    'module_url'        => $module->module_url,
+    'jquery_path'       => $module->jquery_path
+);
+
+$output = $modx->parseText($tpl, $placeholders);
+echo $output;
