ff3a7e4a3d241a219844eaeb0b1755d100b6cec4
cgehuzi <mail.cgehuzi@gmail.com>
2019-08-20 17:46:59 +0300

    [ front ] - рядовые правки фронта модуля

--- assets/modules/RequestManager/ajax.RequestManager.php
+++ assets/modules/RequestManager/ajax.RequestManager.php
@@ -9,13 +9,7 @@ switch ($_GET['q']) {
         $pk = $_POST['pk'];
         $name = $_POST['name'];
         $value = $_POST['value'];
-
-        if (!empty($value)) {
-            echo ($_POST);
-        } else {
-            header('HTTP/1.0 400 Bad Request', true, 400);
-            echo "This field is required!";
-        }
+        echo ($_POST);
 
         die();
         break;
