47d7defac1178a7d36a090a1a9f64600d0e407d7
hkyss <hkyss.off@gmail.com>
2019-08-26 12:04:57 +0300

    удаление комментариев и добавление проверки $_POST

--- assets/modules/RequestManager/ajax.RequestManager.php
+++ assets/modules/RequestManager/ajax.RequestManager.php
@@ -6,30 +6,16 @@ if (empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQU
 switch ($_GET['q']) {
 
     case 'request_comment':
-
-        // Изменение комментария к заявке
-        //$pk = $_POST['pk'];
-        //$name = $_POST['name'];
-        //$value = $_POST['value'];
-		$modx->db->update(array('employee_comment'=>$_POST['value']),$modx->getFullTableName('requestmanager_table'), 'id IN ('.$_POST['pk'].')');
-
-        // Вывод поступившей инфы в лог админки (для отладки)
-        //$modx->logEvent(1, 1, json_encode($_POST['value'], JSON_UNESCAPED_UNICODE), 'request_comment');
-
+        if(isset($_POST['pk']) && isset($_POST['value'])) {
+            $modx->db->update(array('employee_comment'=>$_POST['value']),$modx->getFullTableName('requestmanager_table'), 'id IN ('.$_POST['pk'].')');
+        }
         die();
         break;
 
     case 'request_status':
-
-        // Изменение статуса заявки
-        //$pk = $_POST['pk'];
-        //$name = $_POST['name'];
-        //$value = $_POST['value'];
-		$modx->db->update(array('status'=>$_POST['value']),$modx->getFullTableName('requestmanager_table'), 'id IN ('.$_POST['pk'].')');
-
-        // Вывод поступившей инфы в лог админки (для отладки)
-       	//$modx->logEvent(1, 1, json_encode($_POST, JSON_UNESCAPED_UNICODE), 'request_status');
-
+        if(isset($_POST['pk']) && isset($_POST['value'])) {
+            $modx->db->update(array('status'=>$_POST['value']),$modx->getFullTableName('requestmanager_table'), 'id IN ('.$_POST['pk'].')');
+        }
         die();
         break;
 
