60f5bdd6bf06afca4e35b3c640d353f2e8615bb3
cgehuzi <mail.cgehuzi@gmail.com>
2019-08-27 13:29:00 +0300

    [ delete ] - правит функционал удаления записей

--- assets/modules/RequestManager/ajax.RequestManager.php
+++ assets/modules/RequestManager/ajax.RequestManager.php
@@ -6,18 +6,15 @@ if (empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQU
 switch ($_GET['q']) {
 
     case 'request_comment':
-        if(isset($_POST['pk']) && isset($_POST['value'])) {
-            $modx->db->update(array('employee_comment'=>$_POST['value']),$modx->getFullTableName('requestmanager_table'), 'id IN ('.$_POST['pk'].')');
-        }
+        $modx->db->update(array('employee_comment' => $_POST['value']), $modx->getFullTableName('requestmanager_table'), 'id IN (' . $_POST['pk'] . ')');
         die();
         break;
 
     case 'request_status':
-        if(isset($_POST['pk']) && isset($_POST['value'])) {
-            $modx->db->update(array('status'=>$_POST['value']),$modx->getFullTableName('requestmanager_table'), 'id IN ('.$_POST['pk'].')');
-        }
+        $modx->db->update(array('status' => $_POST['value']), $modx->getFullTableName('requestmanager_table'), 'id IN (' . $_POST['pk'] . ')');
         die();
         break;
+
 
     default:
         die();
