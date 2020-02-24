8cf3c90d684ca586b27517b259edd8a9439065a8
cgehuzi <mail.cgehuzi@gmail.com>
2020-02-24 10:54:29 +0300

    [ fix ] - back to worked version

--- assets/modules/RequestManager/ajax.RequestManager.php
+++ assets/modules/RequestManager/ajax.RequestManager.php
@@ -6,7 +6,7 @@ if (empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQU
 switch ($_GET['q']) {
 
     case 'request_comment':
-        $modx->db->update(array('manager_comment' => $_POST['value']), $modx->getFullTableName('requestmanager_table'), 'id IN (' . $_POST['pk'] . ')');
+        $modx->db->update(array('employee_comment' => $_POST['value']), $modx->getFullTableName('requestmanager_table'), 'id IN (' . $_POST['pk'] . ')');
         die();
         break;
 
--- assets/modules/RequestManager/snippet.RequestManagerSend.php
+++ assets/modules/RequestManager/snippet.RequestManagerSend.php
@@ -1,25 +1,28 @@
 <?
 $relative_path = '';
 $files_arr = $FormLister->getFormData('files');
-if (isset($files_arr['file']) && $files_arr['file']['error'] === 0) {
+if (isset($files_arr['resume__file']) && $files_arr['resume__file']['error'] === 0) {
 	$file_directory = 'assets/files/resume/';
-	$filename = $FormLister->fs->takeFileName($files_arr['file']['name']);
-	$format = $FormLister->fs->takeFileExt($files_arr['file']['name']);
+	$filename = $FormLister->fs->takeFileName($files_arr['resume__file']['name']);
+	$format = $FormLister->fs->takeFileExt($files_arr['resume__file']['name']);
 	$filename = $modx->stripAlias($filename).'.'.$format;
 	$filename = $FormLister->fs->getInexistantFilename($file_directory.$filename,true);
-	if ($FormLister->fs->makeDir($file_directory) && copy($files_arr['file']['tmp_name'],$filename)) {
+	if ($FormLister->fs->makeDir($file_directory) && copy($files_arr['resume__file']['tmp_name'],$filename)) {
         $relative_path = $FormLister->fs->relativePath($filename);
-        $FormLister->setField('path',$relative_path);
+        $FormLister->setField('resume__path',$relative_path);
     }
 }
 
 $fields = array(
     'date'  => date('Y/m/d'),
-    'name' => $data['name'],
-    'email' => $data['email'],
-    'phone' => $data['phone'],
-    'comment' => $data['comment'],
+    'city' => $data['resume__city'],
+    'vacancy'  => $data['resume__pagetitle'],
+    'name' => $data['resume__name'],
+    'email' => $data['resume__email'],
+    'phone' => $data['resume__phone'],
+    'comment' => $data['resume__comment'],
     'file' => $relative_path,
+    'employee_comment' => '',
     'status' => 'opened'
 );
 $modx->db->insert($fields, $modx->getFullTableName('requestmanager_table'));
